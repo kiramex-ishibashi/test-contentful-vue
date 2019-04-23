a6cb83fb03e790d7646c499e1b064fa39bdd89a9
hiromitsu ishibashi <hiromitsu.ishibashi@kiramexi.com>
2019-04-23 11:35:33 +0900

    test

--- src/shared/core.js
+++ src/shared/core.js
@@ -1,6 +1,6 @@
 import * as contentful from 'contentful';
 import dotenv from 'dotenv';
-
+console.log(process.env)
 export const client = contentful.createClient({
   space: process.env.VUE_APP_CONTENTFUL_SPACE,
   accessToken: process.env.VUE_APP_CONTENTFUL_ACCESSTOKEN,
--- src/shared/filters.js
+++ src/shared/filters.js
@@ -1,4 +1,3 @@
 export function formatDate(value) {
-  console.log("value", value)
   return value ? value.split('T')[0].split('-').join('.') : '0000.00.00';
 }
