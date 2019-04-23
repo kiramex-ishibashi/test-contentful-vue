3be9584603f28d0203c037184b63582f49e768b8
hiromitsu ishibashi <hiromitsu.ishibashi@kiramexi.com>
2019-04-23 14:27:40 +0900

    history

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
+const history = require('connect-history-api-fallback');
+
+app.use(history());
 app.use(express.static(__dirname + "/dist/"));
 app.listen(port);
